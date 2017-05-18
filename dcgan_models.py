6e171fa7fb52966100a86db6cf292c5c153d6335
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2017-05-19 01:37:02 +0900

    add files

---
+++ dcgan_models.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Reshape
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling2D, Conv2D, MaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.optimizers import Adam
+from keras.layers import Flatten, Dropout
+
+def generator(input_dim=100,units=1024,activation=Activation('relu')):
+    model = Sequential()
+    model.add(Dense(input_dim=input_dim, units=units))
+    model.add(BatchNormalization())
+    model.add(activation)
+    model.add(Dense(128*7*7))
+    model.add(BatchNormalization())
+    model.add(activation)
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7,)))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(activation)
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+    print(model.summary())
+    return model
+
+def discriminator(input_shape=(28, 28, 1),nb_filter=64,activation=LeakyReLU(0.2)):
+    model = Sequential()
+    model.add(Conv2D(nb_filter, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(activation)
+    model.add(Conv2D(2*nb_filter, (5, 5), strides=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(activation)
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(4*nb_filter))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(activation)
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    print(model.summary())
+    return model
+
---
+++ train.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, sys
+from keras.datasets import mnist
+from PIL import Image
+from dcgan_models import discriminator, generator
+from keras.models import Sequential
+from keras.optimizers import SGD, Adam
+from keras.layers import Activation
+from keras.layers.advanced_activations import ELU
+from visualizer import *
+
+BATCH_SIZE = 32
+NUM_EPOCH = 100
+LR = 0.0002  # initial learning rate
+B1 = 0.5  # momentum term
+GENERATED_IMAGE_PATH = 'images/'
+GENERATED_MODEL_PATH = 'models/'
+
+def train():
+    (X_train, y_train), (_, _) = mnist.load_data()
+    # normalize images
+    X_train = (X_train.astype(np.float32) - 127.5)/127.5
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
+
+    g = generator()
+    d = discriminator(activation=ELU())
+
+    opt = Adam(lr=LR,beta_1=B1)
+    d.trainable = True
+    d.compile(loss='binary_crossentropy',
+              metrics=['accuracy'],
+              optimizer=opt)
+    d.trainable = False
+    dcgan = Sequential([g, d])
+    opt= Adam(lr=lr,beta_1=B1)
+    dcgan.compile(loss='binary_crossentropy',
+                  metrics=['accuracy'],
+                  optimizer=opt)
+
+    num_batches = int(X_train.shape[0] / BATCH_SIZE)
+    # create directory
+    if not os.path.exists(GENERATED_IMAGE_PATH):
+        os.mkdir(GENERATED_IMAGE_PATH)
+    # if not os.path.exists(GENERATED_MODEL_PATH):
+    #     os.mkdir(GENERATED_MODEL_PATH)
+
+    print("-------------------")
+    print('Number of batches:', num_batches)
+    print("-------------------")
+    for epoch in range(NUM_EPOCH):
+
+        for index in range(num_batches):
+            X_g = np.array([np.random.normal(0,0.5,100) for _ in range(BATCH_SIZE)])
+            y_g = [1]*BATCH_SIZE
+            noise = np.array([np.random.normal(0,0.5,100) for _ in range(BATCH_SIZE)])
+            generated_images = g.predict(noise, verbose=0)
+
+            X_d_true = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
+            X_d_gen  = generated_images
+            y_d_true = [1]*BATCH_SIZE
+            y_d_gen = [0]*BATCH_SIZE
+
+            # train discriminator
+            d_loss = d.train_on_batch(X_d_true, y_d_true)
+            d_loss = d.train_on_batch(X_d_gen, y_d_gen)
+            # train generator
+            g_loss = dcgan.train_on_batch(X_g, y_g)
+            sys.stdout.write("\repoch: %d, batch: %d, g_loss: %f, d_loss: %f, g_accuracy: %f, d_accuracy: %f" %
+                    (epoch, index, g_loss[0], d_loss[0], g_loss[1], d_loss[1]))
+            sys.stdout.flush()
+
+        # output generated image
+        image = combine_images(generated_images)
+        image = image*127.5 + 127.5
+        Image.fromarray(image.astype(np.uint8))\
+            .save(GENERATED_IMAGE_PATH+"%03depoch.png" % (epoch))
+        print()
+        # g.save(GENERATED_MODEL_PATH+'dcgan_generator.h5')
+        # d.save(GENERATED_MODEL_PATH+'dcgan_discriminator.h5')
+
+if __name__ == '__main__':
+    train()
