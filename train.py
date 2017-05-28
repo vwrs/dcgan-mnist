4f0f4ab39b36e42ca7827910752922cb870aa2c0
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2017-05-28 12:20:18 +0900

    functionalize

--- train.py
+++ train.py
@@ -6,8 +6,6 @@ from PIL import Image
 from model import discriminator, generator
 from keras.models import Sequential
 from keras.optimizers import SGD, Adam
-from keras.layers import Activation
-from keras.layers.advanced_activations import ELU
 from visualizer import *
 
 BATCH_SIZE = 32
@@ -17,14 +15,18 @@ B1 = 0.5  # momentum term
 GENERATED_IMAGE_PATH = 'images/'
 GENERATED_MODEL_PATH = 'models/'
 
+def show_progress(e,i,g0,d0,g1,d1):
+    sys.stdout.write("\repoch: %d, batch: %d, g_loss: %f, d_loss: %f, g_accuracy: %f, d_accuracy: %f" % (e,i,g0,d0,g1,d1))
+    sys.stdout.flush()
 def train():
     (X_train, y_train), (_, _) = mnist.load_data()
     # normalize images
     X_train = (X_train.astype(np.float32) - 127.5)/127.5
     X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1)
 
+    # build GAN
     g = generator()
-    d = discriminator(activation=ELU())
+    d = discriminator()
 
     opt = Adam(lr=LR,beta_1=B1)
     d.trainable = True
@@ -42,42 +44,38 @@ def train():
     # create directory
     if not os.path.exists(GENERATED_IMAGE_PATH):
         os.mkdir(GENERATED_IMAGE_PATH)
-    # if not os.path.exists(GENERATED_MODEL_PATH):
-    #     os.mkdir(GENERATED_MODEL_PATH)
+    if not os.path.exists(GENERATED_MODEL_PATH):
+        os.mkdir(GENERATED_MODEL_PATH)
 
     print("-------------------")
-    print('Number of batches:', num_batches)
+    print("Total epoch:", NUM_EPOCH, "Number of batches:", num_batches)
     print("-------------------")
-    for epoch in range(NUM_EPOCH):
-
+    z_pred = np.array([np.random.uniform(-1,1,100) for _ in range(BATCH_SIZE)])
+    y_g = [1]*BATCH_SIZE
+    y_d_true = [1]*BATCH_SIZE
+    y_d_gen = [0]*BATCH_SIZE
+    for epoch in list(map(lambda x: x+1,range(NUM_EPOCH))):
         for index in range(num_batches):
-            X_g = np.array([np.random.normal(0,0.5,100) for _ in range(BATCH_SIZE)])
-            y_g = [1]*BATCH_SIZE
-            noise = np.array([np.random.normal(0,0.5,100) for _ in range(BATCH_SIZE)])
-            generated_images = g.predict(noise, verbose=0)
-
             X_d_true = X_train[index*BATCH_SIZE:(index+1)*BATCH_SIZE]
-            X_d_gen  = generated_images
-            y_d_true = [1]*BATCH_SIZE
-            y_d_gen = [0]*BATCH_SIZE
+            X_g = np.array([np.random.normal(0,0.5,100) for _ in range(BATCH_SIZE)])
+            X_d_gen = g.predict(X_g, verbose=0)
 
             # train discriminator
             d_loss = d.train_on_batch(X_d_true, y_d_true)
             d_loss = d.train_on_batch(X_d_gen, y_d_gen)
             # train generator
             g_loss = dcgan.train_on_batch(X_g, y_g)
-            sys.stdout.write("\repoch: %d, batch: %d, g_loss: %f, d_loss: %f, g_accuracy: %f, d_accuracy: %f" %
-                    (epoch, index, g_loss[0], d_loss[0], g_loss[1], d_loss[1]))
-            sys.stdout.flush()
+            show_progress(epoch,index,g_loss[0],d_loss[0],g_loss[1],d_loss[1])
 
-        # output generated image
-        image = combine_images(generated_images)
+        # save generated images
+        image = combine_images(g.predict(z_pred))
         image = image*127.5 + 127.5
         Image.fromarray(image.astype(np.uint8))\
             .save(GENERATED_IMAGE_PATH+"%03depoch.png" % (epoch))
         print()
-        # g.save(GENERATED_MODEL_PATH+'dcgan_generator.h5')
-        # d.save(GENERATED_MODEL_PATH+'dcgan_discriminator.h5')
+        # save models
+        g.save(GENERATED_MODEL_PATH+'dcgan_generator.h5')
+        d.save(GENERATED_MODEL_PATH+'dcgan_discriminator.h5')
 
 if __name__ == '__main__':
     train()
